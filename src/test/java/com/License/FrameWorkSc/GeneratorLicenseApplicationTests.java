f1e694ba5bba13854be245d550c74778face8791
soufien <s.abdennadher@hotmail.com>
2025-11-29 12:28:38 +0100

    first commit v0

---
+++ src/test/java/com/License/FrameWorkSc/GeneratorLicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.License.FrameWorkSc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorLicenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
